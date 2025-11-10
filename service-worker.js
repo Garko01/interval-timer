b2f7510ff73935a7e84d6f0e440684b15786f9bd
Garko01 <garreth.co@gmail.com>
2025-11-10 15:09:32 +0800

    Build for GitHub Pages deployment

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'interval-timer-cache-v1'
+const BASE_PATH = '/interval-timer/'
+const ASSETS = [
+  BASE_PATH,
+  `${BASE_PATH}index.html`,
+  `${BASE_PATH}manifest.webmanifest`,
+  `${BASE_PATH}vite.svg`
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS)
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+          return undefined
+        })
+      )
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone)
+          })
+          return response
+        })
+        .catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('/')
+          }
+          return new Response('Offline', {
+            status: 503,
+            statusText: 'Service Unavailable'
+          })
+        })
+    })
+  )
+})
